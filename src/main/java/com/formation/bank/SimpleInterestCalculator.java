4d251804c1a0d994bce923aede42a8128e765600
Alexandre DELTOUR <alexandre.deltour@geodis.com>
2022-01-04 12:45:15 +0100

    duplicate3

--- src/main/java/com/formation/bank/SimpleInterestCalculator.java
+++ src/main/java/com/formation/bank/SimpleInterestCalculator.java
@@ -1,25 +1,24 @@
 package com.formation.bank;
 
 public class SimpleInterestCalculator implements InterestCalculator {
-	
-	private double rate;
 
-	@Override
-	public void setRate(double rate) {
-		this.rate=rate;
+    private double rate;
+    public String ratename;
 
-	}
+    @Override
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
 
-	@Override
-	public double calculate(double amount, double year) {
-		if(amount<0||year <0) {
-			throw new IllegalArgumentException("Montant et durée doivent être positifs");
-		}
-		if(amount<0||year <0) {
-			throw new IllegalArgumentException("Montant et durée doivent être positifs");
-		}
-		
-		return amount*year*rate;
-	}
+    @Override
+    public double calculate(double amount, double year) {
+        if(amount < 0 || year < 0) {
+            throw new IllegalArgumentException(
+                    "Le monmtant et la duree doivent etre positifs");
+        }
+
+
+        return amount * year * rate;
+    }
 
 }
